42ff884a7f971fa579cb0b16808358c4e9b9ae59
hassanzadeh20 <52526228+hassanzadeh20@users.noreply.github.com>
2020-09-16 20:00:25 +0200

    GCP connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000'
+  //apiUrl: 'http://localhost:4000'
+  apiUrl: 'https://snitt-test-v1.uc.r.appspot.com'
 };
 
 /*
